363fc0b7fd47e62640594a9ff0e5ef43284d961c
faisa1khan <faisalkhan5464@gmail.com>
2019-11-23 14:48:09 +0530

    class components

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
 
+const getStateFromLocaleStorage = () => {
+  const storage = localStorage.getItem('counterState');
+  if (storage) return JSON.parse(storage);
+  return { count: 0 };
+};
+
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getStateFromLocaleStorage();
+
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+    this.increment = this.increment.bind(this);
+    this.updateDocumentTitle = this.updateDocumentTitle.bind(this);
+  }
+  updateDocumentTitle() {
+    document.title = this.state.count;
+  }
+  increment() {
+    this.setState(
+      (state, props) => {
+        const { max, step } = props;
+        if (state.count >= max) return;
+        return { count: state.count + step };
+      },
+      () => {
+        this.updateDocumentTitle();
+      },
+    );
+  }
+  decrement() {
+    this.setState(
+      (state, props) => {
+        const { step } = props;
+        if (state.count === 0) return;
+        return { count: state.count - step };
+      },
+      () => {
+        this.updateDocumentTitle();
+      },
+    );
+  }
+  reset() {
+    this.setState({ count: 0 });
+  }
   render() {
+    const { count } = this.state;
     return (
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     );
