d64546891f703b77d34247f2b816fa0312cf17b4
faisa1khan <faisalkhan5464@gmail.com>
2019-11-23 16:11:28 +0530

    need to fix localStorage

--- src/Counter.js
+++ src/Counter.js
@@ -1,31 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-// const getStateFromLocaleStorage = () => {
-//   const storage = localStorage.getItem('counterState');
-//   if (storage) return JSON.parse(storage);
-//   return { count: 0 };
-// };
+const getStorageFromLocalStorage = () => {
+  const storage = localStorage.getItem('counterState');
+  console.log(storage);
+  if (storage) return JSON.parse(storage).count;
+  return { count: 0 };
+};
+
+const storeStateInLocalStorage = count => {
+  localStorage.setItem('count', JSON.stringify({ count }));
+  console.log(localStorage);
+};
+
+const useLocalStorage = (initialState, key) => {
+  const get = () => {
+    const storage = localStorage.getItem(key);
+    if (storage) return JSON.parse(storage)[value];
+    return initialState;
+  };
+
+  const [value, setValue] = useState(get());
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify({ value }));
+  }, [value, key]);
+
+  return [value, setValue];
+};
 
 const Counter = ({ max, step }) => {
-  const intialValue = window.localStorage.getItem('count') || 0;
-  const [count, setCount] = useState(intialValue);
+  const [count, setCount] = useState(0);
+  const countRef = useRef();
+  console.log(countRef);
+
+  let message = '';
+
+  if (countRef.current < count) message = 'Higher';
+  if (countRef.current > count) message = 'Lower';
+
+  countRef.current = count;
 
   const increment = () => {
     setCount(c => {
-      if (c >= max) return c;
-      return c + step;
+      return c + 1;
     });
   };
   const decrement = () => setCount(count - 1);
   const reset = () => setCount(0);
 
   useEffect(() => {
-    document.title = `Counter: ${count}`;
-    window.localStorage.setItem('count', count);
+    const id = setInterval(() => {
+      console.log(`Count: ${count}`);
+    }, 3000);
+
+    return () => clearInterval(id);
   }, [count]);
 
   return (
     <div className="Counter">
+      <p>{message}</p>
       <p className="count">{count}</p>
       <section className="controls">
         <button onClick={increment}>Increment</button>
